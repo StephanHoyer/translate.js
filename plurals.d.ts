c43bead4989b8b86de02d5d6cdc4deb29116b49d
Kurounin <wiseon3@yahoo.com>
2018-10-12 11:15:43 +0300

    Added plurals typings

---
+++ plurals.d.ts
@@ -0,0 +1,13 @@
+type plural = (n: number, translationKey: string) => string;
+
+export const plural_CS: plural;
+export const plural_DA: plural;
+export const plural_DE: plural;
+export const plural_EN: plural;
+export const plural_ES: plural;
+export const plural_FR: plural;
+export const plural_IS: plural;
+export const plural_IT: plural;
+export const plural_JA: plural;
+export const plural_PT: plural;
+export const plural_SE: plural;
