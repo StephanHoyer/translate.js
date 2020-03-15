d10c08191855349f6cb056b79c156acee9a338c7
mar <mar.nospam@anomy.net>
2020-03-15 22:19:28 +0000

    fix: Allow passing numbers into pluralizations, support wildcard subkeys

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,22 +4,30 @@
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 // TypeScript Version: 2.8
 
-type translateFunc1<M extends object, T> = <K extends keyof M>(
+interface WildCard {
+  '*': string
+}
+
+type translateFunc1<M extends Messages, T> = <K extends keyof M>(
   key: M[K] extends string ? K : never
 ) => T
-type translateFunc2<M extends object, T> = <K extends keyof M>(
+type translateFunc2<M extends Messages, T> = <K extends keyof M>(
   key: K,
   params: M[K] extends string ? object : never
 ) => T
-type translateFunc3<M extends object, T> = <K extends keyof M>(
+type translateFunc3<M extends Messages, T> = <K extends keyof M>(
   key: K,
-  subKey: M[K] extends object ? keyof M[K] : number,
+  subKey: M[K] extends object
+    ? (M[K] extends WildCard ? string : keyof M[K]) | number
+    : number,
   params?: object
 ) => T
-type translateFunc4<M extends object, T> = <K extends keyof M>(
+type translateFunc4<M extends Messages, T> = <K extends keyof M>(
   key: K,
   params: object,
-  subKey: M[K] extends object ? keyof M[K] : number
+  subKey: M[K] extends object
+    ? (M[K] extends WildCard ? string : keyof M[K]) | number
+    : number
 ) => T
 
 import { pluralizer } from './plurals'
@@ -37,12 +45,11 @@ export interface ArrayOptions extends Options {
 }
 
 export interface Messages {
-  [key: string]: string | Messages
-  [key: number]: string | Messages
+  [key: string]: string | Record<string | number, string>
 }
 
 export type Translate<
-  M extends object,
+  M extends Messages,
   T extends ArrayOptions | Options,
   R = T extends ArrayOptions ? any[] : string
 > = {
--- src/type-tests.ts
+++ src/type-tests.ts
@@ -1,15 +1,31 @@
 // This file is only to check if the TypeScript definitions are behaving ok.
 
 import translate from './index'
+import { plural_EN } from './plurals'
 
-const t = translate({
+const opts = { pluralize: plural_EN }
+
+const messages = {
   greeting: 'Hello',
   defaultName: 'World',
   foo: {
     bar: 'asdf',
   },
+  foot: {
+    1: 'bar',
+    2: 'baz',
+  },
   foos: 'bhasd',
-})
+  results: {
+    s: '{n} result for „{q}“',
+    p: '{n} results for „{q}“',
+  } as Record<'s' | 'p', string>,
+  wildcard: {
+    '*': 'default',
+  },
+}
+
+const t = translate(messages, opts)
 
 let s: string
 
@@ -18,6 +34,10 @@ s = t('foo', 'bar', { n: 2 })
 s = t('foo', { n: 2 }, 'bar')
 s = t('defaultName')
 s = t('foos', { n: 2 })
+s = t('results', 3)
+s = t('results', 3, { q: 'foo' })
+s = t('results', { q: 'foo' }, 3)
+s = t('wildcard', 'foo')
 
 // ExpectError
 t('foo', 'barf', { n: 2 })
